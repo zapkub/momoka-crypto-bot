e054e1836c723aadb8e2f7e8c4a78353edccc372
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-09-16 01:45:08 +0700

    feat(handle error on api call)

--- adapter/messenger/adapter.js
+++ adapter/messenger/adapter.js
@@ -3,10 +3,17 @@ const CryptowatAdapter = require('../exchange/cryptowat.adapter')
 const arbitageStrategy = require('../../strategy/arbitage.strategy')
 const actions = require('../../parser/actions')
 
+class Notification {
+  constructor ({ receptionId, action }) {
+    this.__receptionId = receptionId
+    this.__action = action
+  }
+}
 class MessengerAdapter {
   constructor () {
     this.bx = new BxAdapter()
     this.cryptowat = new CryptowatAdapter()
+    this.notificationList = []
   }
   getPrice (currency, compare) {
     compare = compare.toLowerCase()
@@ -16,25 +23,41 @@ class MessengerAdapter {
       return this.cryptowat.getPriceByCurrencyPrefix(currency, compare)
     }
   }
+
+  // Response message generate here
   async getResponseMessage (action) {
     console.log('get response with action')
     console.log(action)
     switch (action.type) {
       case actions.GET_PRICE:
-        const result = await this.getPrice(action.payload.currency, action.payload.compare)
-        return {
-          type: 'text',
-          text: `ราคา ${result.secondaryCurrency.toUpperCase()} ตอนนี้ ${result.value} ${result.primaryCurrency} ค่ะ`
+        try {
+          const result = await this.getPrice(action.payload.currency, action.payload.compare)
+          return {
+            type: 'text',
+            text: `ราคา ${result.secondaryCurrency.toUpperCase()} ตอนนี้ ${result.value} ${result.primaryCurrency} ค่ะ`
+          }
+        } catch (e) {
+          return {
+            type: 'text',
+            text: 'ไม่สามารถเช็คราคาได้ในขณะนี้ กรุณาลองใหม่ค่ะ'
+          }
         }
       case actions.GET_ARBITAGE_PRICE: {
-        const result = await arbitageStrategy.getArbitagePriceByCurrencyList(['omg', 'btc', 'xrp', 'eth'])
-        const worthResult = result.prices.map(price => `${price.currency} แพงกว่า ${-price.margin.toFixed(3)} THB (${-price.marginPercent.toFixed(2)}%)\n`)
-        return {
-          type: 'text',
-          text: `ราคาตลาดเทียบระหว่าง bx กับ Bifinex\n` +
+        try {
+          const result = await arbitageStrategy.getArbitagePriceByCurrencyList(['omg', 'btc', 'xrp', 'eth'])
+          const worthResult = result.prices.map(price => `${price.currency} แพงกว่า ${-price.margin.toFixed(3)} THB (${-price.marginPercent.toFixed(2)}%)\n`)
+          return {
+            type: 'text',
+            text: `ราคาตลาดเทียบระหว่าง bx กับ Bifinex\n` +
           `ค่าเงิน 1 USD ต่อ ${result.thbusd}  THB\n` +
           worthResult.join('')
 
+          }
+        } catch (e) {
+          return {
+            type: 'text',
+            text: `เกิดข้อผิดพลาดระหว่างเทียบราคา กรุณาลองใหม่ค่ะ`
+          }
         }
       }
       case actions.AWAKE:
