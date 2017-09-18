483efe96366092d30d474877b548d67f0b1a31a6
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-09-18 23:11:46 +0700

    chore(gitignore): remove .dist folder from git

--- .bin/deploy.bash
+++ .bin/deploy.bash
@@ -3,6 +3,6 @@ echo 'Deploy Momoka to '$DEPLOY_TARGET
 yarn jest
 rm -rf ./dist
 mkdir -p dist
-tar --exclude='.git' --exclude='.env' --exclude='./dist/artifact.tar' --exclude='./node_modules' --exclude='./.vscode' -czf ./dist/artifact.tar ./
+tar --exclude='.git' --exclude='.env' --exclude='./dist/artifact.tar' --exclude='./.db' --exclude='./node_modules' --exclude='./.vscode' -czf ./dist/artifact.tar ./
 rsync -avz ./dist/artifact.tar $DEPLOY_TARGET:/root/momoka-release.tar
 ssh -t $DEPLOY_TARGET 'cd /root && mkdir -p momoka && tar -xvf momoka-release.tar -C /root/momoka && cd momoka && yarn install'
