f2523a1884f9b651db37a10908bd65a0aba01b69
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2017-09-28 08:56:36 +0700

    fix(notification): remove ref id from price strategy notification

--- strategy/price.strategy.js
+++ strategy/price.strategy.js
@@ -55,8 +55,7 @@ module.exports = {
         type: 'text',
         text:
           `แจ้งเตือน ${command} ${conditionResult.text} ${condition.value}\n` +
-          `ตอนนี้ ${result.value} แล้วค่ะ 😌\n` +
-          `ref. ${_id}`
+          `ตอนนี้ ${result.value} แล้วค่ะ 😌\n`
       }
     }
   },
