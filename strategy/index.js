badc49590d283f6ea01a545d4c10666b7a1c01a0
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-11-26 15:55:12 +0700

    update strategy index.js

--- strategy/index.js
+++ strategy/index.js
@@ -1,10 +1,8 @@
 const arbitageStrategy = require('./arbitage.strategy')
 const priceStrategy = require('./price.strategy')
-const airport = require('./airports.strategy')
 
 module.exports = [
   priceStrategy,
   arbitageStrategy.getArbitagePriceListStrategy,
-  arbitageStrategy.getArbitagePriceStrategy,
-  airport.metarStrategy
+  arbitageStrategy.getArbitagePriceStrategy
 ]
