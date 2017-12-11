d7085cfd83deffa16e3301efeb383fad402ef742
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-12-11 17:54:50 +0700

    style(comment): add comment for usd adapter

--- exchange/cryptowat.adapter.js
+++ exchange/cryptowat.adapter.js
@@ -10,16 +10,35 @@ class CryptowatAdapter extends ExchangeAdapter {
 
   async getPriceByCurrencyPrefix (currency, compare) {
     console.log(chalk.blue(`Cryptowat Adapter: ${currency}${compare}`))
+
+    /**
+     * Currency compare here
+     * convert both to lowercase
+     */
     currency = currency.toLowerCase()
     compare = compare.toLowerCase()
-    const targetUrl = this.API_ENDPOINT + `markets/bitfinex/${currency}${compare}/price`
-    const priceInfo = await this.fetchDataToCache(targetUrl)
-    console.log(chalk.green(`Cryptowat Adapter: ${priceInfo.result.price}`))
-    return {
-      origin: 'finex',
-      primaryCurrency: compare,
-      secondaryCurrency: currency,
-      value: priceInfo.result.price
+    // get infomation from bitfinex
+    try {
+      /**
+       * Need to implement logic for crypto that
+       * isn't listed in bitfinex by changing
+       * this.API_ENDPOINT to another exchange
+       * eg.
+       * if(currency === 'xzc')
+       */
+
+      const targetUrl =
+        this.API_ENDPOINT + `markets/bitfinex/${currency}${compare}/price`
+      const priceInfo = await this.fetchDataToCache(targetUrl)
+      console.log(chalk.green(`Cryptowat Adapter: ${priceInfo.result.price}`))
+      return {
+        origin: 'finex',
+        primaryCurrency: compare,
+        secondaryCurrency: currency,
+        value: priceInfo.result.price
+      }
+    } catch (e) {
+      // handle if error soon
     }
   }
 }
